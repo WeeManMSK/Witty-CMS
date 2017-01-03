370c55cfbf3c4f2baf8a1c852000a2ffc51eee04
WeeManMSK <teranton@gmail.com>
2017-01-03 07:37:01 +0300

    Witty_CMS-7 Menu updated

--- backend/helper/Menu.php
+++ backend/helper/Menu.php
@@ -1,17 +1,17 @@
 <?php namespace backend\helper;
 
-use Yii;
+use yii;
 
 class Menu
 {
     public static function Items() {
         return  [
-            ['label' => '<i class="fa fa-dashboard"></i><span>Главная</span>', 'url' => ['/home']],
-            ['label' => '<i class="fa fa-briefcase"></i><span>Контент</span>', 'url' => ['/content'], 'items' => [
-                ['label' => '<i class="fa fa-circle-o"></i>Страницы', 'url' => ['/page']],
-                ['label' => '<i class="fa fa-circle-o"></i>Блог', 'url' => ['/blog']],
+            ['label' => '<i class="fa fa-dashboard"></i><span>Dashboard</span>', 'url' => ['/home']],
+            ['label' => '<i class="fa fa-briefcase"></i><span>Content</span>', 'url' => ['/content'], 'items' => [
+                ['label' => '<i class="fa fa-circle-o"></i>Pages', 'url' => ['/page']],
+                ['label' => '<i class="fa fa-circle-o"></i>Blog', 'url' => ['/blog']],
             ]],
-            ['label' => 'Выход', 'url' => ['/site/logout'], 'visible' => !Yii::$app->user->isGuest],
+            ['label' => '<i class="fa fa-sign-out"></i>Sign out', 'url' => ['/site/logout'], 'visible' => !Yii::$app->user->isGuest],
         ];
     }
 }
