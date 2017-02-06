40a383e8d8d8b340ed35ade27656a6bd0f189fc6
WeeManMSK <teranton@gmail.com>
2017-02-06 06:33:07 +0300

    Add db migrations

--- console/migrations/m170206_032732_create_catalog_item_attribute_type_table.php
+++ console/migrations/m170206_032732_create_catalog_item_attribute_type_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%catalog_items_attribute_type}}`.
  */
-class m170206_032732_create_catalog_item_attribute_type_tables_attribute_type_table extends Migration
+class m170206_032732_create_catalog_item_attribute_type_table extends Migration
 {
     /**
      * @inheritdoc
